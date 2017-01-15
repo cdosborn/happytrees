41396b6fc8493e3445bd86a13d78bbb243cb3e5b
Sean Stephens <seanastephens@email.arizona.edu>
2017-01-15 04:48:16 -0700

    cloud perf

--- js/clouds.js
+++ js/clouds.js
@@ -1,29 +1,24 @@
-function poly(xr, yr) {
-    const p = (x,y) => ({x,y});
-    const points = [ p(-xr,-yr), p(-xr,yr), p(xr,yr), p(xr,-yr)];
-    return (xshift, yshift) => points.map((p, i) => {
-        return (i == 0 ? 'M' : 'L') + (p.x + xshift) + ' ' + (p.y + yshift);
-    }).join('');
-}
-
 function createCloud(globals) {
   return function(d) {
-    const paths = d3.range(0, Math.floor(Math.random() * 5 + 4)).map(() => poly(2*d.width, 2*d.height));
-    d3.select(this).selectAll('path').data(paths).enter().append('path').classed('outline', true);
+    const rects = d3.range(0, Math.floor(Math.random() * 5 + 4));
+    d3.select(this).selectAll('rect').data(rects).enter().append('rect')
+      .classed('outline', true)
+      .attr('fill', 'white')
+      .attr('opacity', '0.8')
+      .attr('width', 4*d.width)
+      .attr('height', 4*d.height);
   };
 }
 
 function updateCloud(time, delta, globals) {
   return function(d) {
-
     const xShift = ((time / (d.speed * 100)) % (3 * globals.width)) - globals.width;
     const yShift = globals.yScale(d.y);
 
     d3.select(this)
       .selectAll('.outline')
-      .attr('fill', 'white')
-      .attr('opacity', '0.8')
-      .attr('d', (d, i) => d(xShift + 15 * i + Math.random() * 5, yShift + 20 * Math.sin(i * 40) + Math.random() * 5));
+      .attr('x', (d, i) => xShift + 15 * i + Math.random() * 5)
+      .attr('y', (d, i) => yShift + 20 * Math.sin(i * 40) + Math.random() * 5);
 
   };
 }
